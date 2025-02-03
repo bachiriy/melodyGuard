1d4bb2e2fc3b0cee4a84b065632795c4cde672af
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2025-02-03 17:26:52 +0100

    update

--- api/src/main/java/com/melodyguard/api/repository/UserRepository.java
+++ api/src/main/java/com/melodyguard/api/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.melodyguard.api.repository;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.melodyguard.api.model.User;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 	Optional<User> findByUsername(String username);
 
